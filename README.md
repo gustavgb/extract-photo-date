0fcbee39c468d5ee19fa2b32a31a9e2d961ba38f
Gustav Burchardt <gustav@gusgram.dk>
2024-08-23 17:46:49 +0200

    Implement first version

---
+++ README.md
@@ -0,0 +1,9 @@
+# extract-photo-date
+
+A script to prefix each jpeg photo in a folder with the date it was taken.
+
+Useful for storing photos in a cloud provider while preserving sorting by date taken.
+
+## Usage
+
+Clone this repo and install with `npm install`. Then navigate to the folder containing the unprocessed photos and run `extract-photo-date`. Processed photos will be copied to *./output/*.
---
+++ bin/extract-photo-date.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import glob from 'glob'
+import fs from 'fs-extra'
+import path from 'path'
+import dateFormat from 'dateformat'
+import serialExec from 'promise-serial-exec'
+import exif from 'fast-exif'
+import process from 'process'
+
+const inputDirectory = process.cwd()
+const outputDirectory = path.join(inputDirectory, 'output')
+const inputFiles = glob.sync(inputDirectory + '/*.{jpeg,jpg}', { maxDepth: 1 })
+
+async function extractImageDate (filePath) {
+  const exifData = await exif.read(filePath)
+  let date = ''
+
+  if (exifData && exifData.exif && exifData.exif.DateTimeOriginal) {
+    date = dateFormat(exifData.exif.DateTimeOriginal, 'yymmdd')
+  } else {
+    const { mtime } = await fs.stat(filePath)
+    date = dateFormat(mtime, 'yymmdd')
+  }
+
+  if (date) {
+    const newFilePath = path.join(
+      outputDirectory,
+      path.dirname(filePath).replace(inputDirectory, '').replace(/^\//, ''),
+      `${date} - ${path.basename(filePath)}`
+    )
+
+    await fs.copy(filePath, newFilePath)
+  }
+}
+
+(async function () {
+  await serialExec(inputFiles.map((filePath, index) => () => extractImageDate(filePath).then(() =>
+    console.log(
+      `Processed file ${index + 1} / ${inputFiles.length}: ${(
+        ((index + 1) / inputFiles.length) * 100
+      ).toFixed(2)}%`
+    )
+  )))
+})()
